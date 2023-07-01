abd16e799e9a503a9f7f54f126a27fda713e8e61
gaayudevelops <124303954+gaayudevelops@users.noreply.github.com>
2023-07-01 20:58:11 +0530

    The Middlewares Task 02

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 
 const express = require('express');
+const bodyParser=require('body-parser');
 
 const app= express();
 
-app.use((req,res,next)=>{
-    console.log('In the middleware');
-    next(); // Allows the request to continue to the next middleware
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/add-product' , (req,res,next)=>{
+   res.send('<form action="/product" method="POST"><p>Enter the Product name:</p><input type="text" name="Product"><br><p>Enter the Size of the product: </p><input type="text" name="Size"><br><br><button type="submit">Add Product</button></form>');
+   
 })
-app.use((req,res,next)=>{
-    console.log('In another middleware');
-    res.send('<h1>Hello from Express.js</h1>');
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+})
+app.use('/',(req,res,next)=>{
+    res.send('<h1>Hello from Express JS</h1>');
 })
 
 app.listen(3000);
